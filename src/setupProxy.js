642aaf285cbf7f6c8928457c1b5a50a4235d6753
ZZlovezf <zz644361452@163.com>
2018-12-24 08:15:45 +0800

    final

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,7 @@ var proxy = require('http-proxy-middleware');
 
 
 module.exports =(app)=>{
-	app.use('/index/hotsShowList', proxy({target:'https://m.juooo.com/', changeOrigin: true}));
-	app.use('/Show/getShowList', proxy({target:'https://m.juooo.com/', changeOrigin: true}));
-	app.use('/index/hotsShowList', proxy({target: 'https://m.juooo.com/',changeOrigin: true}));
+	app.use('/index', proxy({target:'https://m.juooo.com/', changeOrigin: true}));
 	app.use(proxy('/Show', {target: 'https://m.juooo.com',changeOrigin: true}));
 }
+
